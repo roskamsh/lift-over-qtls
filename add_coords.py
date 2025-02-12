fe57fe209ee057b2dd710d2c70509dd7a2a5480a
roskamsh <breesh.roskamshieter@gmail.com>
2025-02-12 14:53:29 +0000

    add additional allele info

--- add_coords.py
+++ add_coords.py
@@ -12,18 +12,17 @@ def add_genome_coordinates(snp_positions_file, reference_file, original_file, da
 
     ## Now join back with original file
     if (datatype=="eQTLGen"):
-        hg38_coords = merged[["ID","Molecule","POS"]].copy()
-        hg38_coords.columns = ["ID","CHR_hg38","POS_hg38"]
+        hg38_coords = merged[["ID","Molecule","POS","MAJOR","MINOR","REF","ALT"]].copy()
+        hg38_coords.columns = ["SNP","SNPChr","SNPPos","MajorAllele","MinorAllele","REF","ALT"]
         outname=os.path.basename(original_file).replace(".txt","") + '_hg38.txt'
         original=pd.read_csv(original_file,sep="\t")
         original = original.drop(columns=["SNPChr","SNPPos"])
-        hg38_coords.columns = ["SNP","SNPChr","SNPPos"]
         original_annot = original.merge(hg38_coords, on="SNP")
         os.makedirs(os.path.join("data","lifted","eqtls"), exist_ok=True)
         original_annot.to_csv(os.path.join("data","lifted","eqtls",outname), index = False, sep="\t")
     elif (datatype=="baal-nf"):
-        hg38_coords = merged[["ID","Molecule","POS"]].copy()
-        hg38_coords.columns = ["ID","CHR_hg38","POS_hg38"]
+        hg38_coords = merged[["ID","Molecule","POS","MAJOR","MINOR"]].copy()
+        hg38_coords.columns = ["ID","CHR_hg38","POS_hg38","MajorAllele","MinorAllele"]
         directory=original_file 
         for filename in os.listdir(directory):
             if filename.endswith(".csv"):  # Ensure it's a CSV file
@@ -37,7 +36,7 @@ def add_genome_coordinates(snp_positions_file, reference_file, original_file, da
                     filtered_df = df[df["ASB_quality"] == "High"]
                     if not filtered_df.empty:
                         filtered_df = filtered_df.drop(columns=["CHROM","POS"])
-                        hg38_coords.columns = ["ID","CHROM","POS"]
+                        hg38_coords.columns = ["ID","CHROM","POS","MajorAllele","MinorAllele"]
                         updated = filtered_df.merge(hg38_coords, on = "ID")
                         updated["CHROM"] = 'chr' + updated['CHROM'].astype(str) # to exactly match baal-nf output
                         os.makedirs(os.path.join("data","lifted","baal-nf"), exist_ok=True)
@@ -45,8 +44,8 @@ def add_genome_coordinates(snp_positions_file, reference_file, original_file, da
                 else:
                     print(f"Skipping {filename}: Missing required columns")
     elif (datatype=="transactors"):
-        hg19_coords = merged[["ID","Molecule","POS","MAJOR","MINOR"]].copy()
-        hg19_coords.columns = ["ID","CHR_hg19","POS_hg19","MAJOR","MINOR"]
+        hg19_coords = merged[["ID","Molecule","POS","MAJOR","MINOR","REF","ALT"]].copy()
+        hg19_coords.columns = ["ID","CHR_hg19","POS_hg19","MajorAllele","MinorAllele","REF","ALT"]
         directory=original_file
         for tf in os.listdir(directory):
             file=f"{directory}/{tf}/{tf}_transactors_filtered_hg38.txt"
@@ -57,19 +56,19 @@ def add_genome_coordinates(snp_positions_file, reference_file, original_file, da
             updated["AssessedAllele"] = updated["RISK.ALLELE"]
             # Add the OtherAllele column, which is always equal to the other allele
             updated["OtherAllele"] = updated.apply(
-                lambda row: row["MAJOR"] if row["AssessedAllele"] == row["MINOR"] else row["MINOR"],
+                lambda row: row["MajorAllele"] if row["AssessedAllele"] == row["MinorAllele"] else row["MinorAllele"],
                 axis=1
             )
             updated["CHR_POS"] = updated["CHR_POS"].astype(int)
             updated["Pvalue"] = 0 # Set Pvalue column to zero for prioritization in ld-clump pipeline
             updated["GeneSymbol"] = tf
             # write hg19 table with alleles, formatted for ld-clump pipeline
-            hg19_out = updated[['ID', 'CHR_hg19','POS_hg19', 'MAJOR', 'MINOR', 'AssessedAllele', 'OtherAllele','GeneSymbol',
-                                'Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
+            hg19_out = updated[['ID', 'CHR_hg19','POS_hg19', 'MajorAllele', 'MinorAllele','REF','ALT','AssessedAllele', 'OtherAllele',
+                                'GeneSymbol','Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
                                 'MAPPED_TRAIT_URI', 'P.VALUE', 'STUDY', 'STUDY.ACCESSION', 'INITIAL.SAMPLE.SIZE', 
                                 'DATE.ADDED.TO.CATALOG', 'OR.or.BETA', 'PVALUE_MLOG', 'RISK.ALLELE', 'SAMPLE.SIZE']].copy()
-            hg19_out.columns = ['SNPChr','SNPChr','SNPPos','MajorAllele','MinorAllele','AssessedAllele', 'OtherAllele','GeneSymbol',
-                                'Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
+            hg19_out.columns = ['SNPChr','SNPChr','SNPPos','MajorAllele','MinorAllele','REF','ALT','AssessedAllele', 'OtherAllele',
+                                'GeneSymbol','Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
                                 'MAPPED_TRAIT_URI', 'P.VALUE', 'STUDY', 'STUDY.ACCESSION', 'INITIAL.SAMPLE.SIZE', 
                                 'DATE.ADDED.TO.CATALOG', 'OR.or.BETA', 'PVALUE_MLOG', 'RISK.ALLELE', 'SAMPLE.SIZE']
             outname_hg19=os.path.basename(file).replace("_hg38.txt","_hg19_with_alleles.txt")
@@ -77,14 +76,14 @@ def add_genome_coordinates(snp_positions_file, reference_file, original_file, da
             hg19_out.to_csv(os.path.join("data","lifted","transactors",tf,outname_hg19), index=False,sep="\t")
             
             # write hg38 table with alleles, formatted for ld-clump pipeline
-            hg38_out = updated[['ID', 'CHR_ID','CHR_POS', 'MAJOR', 'MINOR', 'AssessedAllele', 'OtherAllele','GeneSymbol',
-                                'Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
+            hg38_out = updated[['ID', 'CHR_hg19','POS_hg19', 'MajorAllele', 'MinorAllele','REF','ALT','AssessedAllele', 'OtherAllele',
+                                'GeneSymbol','Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
                                 'MAPPED_TRAIT_URI', 'P.VALUE', 'STUDY', 'STUDY.ACCESSION', 'INITIAL.SAMPLE.SIZE', 
                                 'DATE.ADDED.TO.CATALOG', 'OR.or.BETA', 'PVALUE_MLOG', 'RISK.ALLELE', 'SAMPLE.SIZE']].copy()
-            hg38_out.columns = ['SNPChr','SNPChr','SNPPos','MajorAllele','MinorAllele','AssessedAllele', 'OtherAllele','GeneSymbol',
-                                'Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
+            hg38_out.columns = ['SNPChr','SNPChr','SNPPos','MajorAllele','MinorAllele','REF','ALT','AssessedAllele', 'OtherAllele',
+                                'GeneSymbol','Pvalue','STRONGEST.SNP.RISK.ALLELE', 'RISK.ALLELE.FREQUENCY', 'DISEASE.TRAIT', 'MAPPED_TRAIT',
                                 'MAPPED_TRAIT_URI', 'P.VALUE', 'STUDY', 'STUDY.ACCESSION', 'INITIAL.SAMPLE.SIZE', 
-                                'DATE.ADDED.TO.CATALOG', 'OR.or.BETA', 'PVALUE_MLOG', 'RISK.ALLELE', 'SAMPLE.SIZE'] 
+                                'DATE.ADDED.TO.CATALOG', 'OR.or.BETA', 'PVALUE_MLOG', 'RISK.ALLELE', 'SAMPLE.SIZE']
             outname_hg38=os.path.basename(file).replace("_hg38.txt","_hg38_with_alleles.txt")
             os.makedirs(os.path.join("data","lifted","transactors",tf), exist_ok=True)
             hg38_out.to_csv(os.path.join("data","lifted","transactors",tf,outname_hg38), index=False, sep="\t")
